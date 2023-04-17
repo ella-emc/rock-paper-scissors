31aef51b0a15874b23cb4f6bc3f227e3ce076d79
Ella Mae <ellalala.mc@gmail.com>
2023-04-17 14:39:06 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,29 @@
+function getComputerChoice() {
+    rand = Math.floor(Math.random() * 3) + 1;   // generates random number from 1 to 3
+    if (rand === 1)
+        return "rock";
+    else if (rand === 2)
+        return "paper";
+    else
+        return "scissors";
+}
+
+function playRound(playerSelection, computerSelection) {
+    playerChoice = playerSelection.toLowerCase();
+
+    // PLAYER WINS
+    if (computerSelection === "rock" && playerChoice === "paper")
+        return "You win! Paper beats rock.";
+    else if (computerSelection === "paper" && playerChoice === "scissors")
+        return "You win! Scissors beat paper.";
+    else if (computerSelection === "scissors" && playerChoice === "rock")
+        return "You win! Rock beats scissors.";
+    
+    // COMPUTER WINS
+    
+}
+
+const computerSelection = getComputerChoice();
+const playerSelection = prompt("Enter your choice (rock, paper, or scissors): ");
+
+
