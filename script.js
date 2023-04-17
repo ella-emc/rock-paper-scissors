72c352e69c546430466b1796efe94345183977e0
Ella Mae <ellalala.mc@gmail.com>
2023-04-17 14:45:33 +0800

    Complete playRound function

--- script.js
+++ script.js
@@ -20,8 +20,18 @@ function playRound(playerSelection, computerSelection) {
         return "You win! Rock beats scissors.";
     
     // COMPUTER WINS
+    else if (computerSelection === "rock" && playerChoice === "scissors")
+        return "You lose! Rock beats scissors.";
+    else if (computerSelection === "paper" && playerChoice === "rock")
+        return "You lose! Paper beats rock.";
+    else if (computerSelection === "scissors" && playerChoice === "paper")
+        return "You lose! Scissors beat paper.";
     
+    // DRAW
+    else 
+        return "It's a draw!";
 }
+
 
 const computerSelection = getComputerChoice();
 const playerSelection = prompt("Enter your choice (rock, paper, or scissors): ");
